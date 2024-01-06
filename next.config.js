e9e3dbff65263c592cb6f2c99fb792a379d11a94
Tacianjr07 <t.juniior13@gmail.com>
2024-01-06 17:23:04 -0300

    edit next.config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["images.pexels.com"], // Adicione o domínio da imagem aqui
+  },
   compiler: {
     styledComponents: true,
   },
