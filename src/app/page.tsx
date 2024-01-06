55ffd54ce084588ea412b61bb986139902753c41
Tacianjr07 <t.juniior13@gmail.com>
2024-01-06 16:49:14 -0300

    :sparkles: feat: using react-hooks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,33 @@
 import Image from "next/image";
 import shoes1 from "../assets/pexels-aman-jakhar-2048548.jpg";
 import { StyledItem, StyledMain, StyledRight, StyledSection } from "./style";
+import { useState, useEffect } from "react";
+import { api } from "@/services/api";
+
+interface IProductsProps {
+  id: number;
+  name: string;
+  cover: string;
+  description: string;
+  price: number;
+}
+
 export default function Home() {
+  useEffect(() => {
+    async function getProducuts() {
+      const response = await api.get("/products");
+      setProducts(response.data);
+    }
+
+    getProducuts();
+  }, []);
+
+  const [products, setProducts] = useState<IProductsProps[]>([]);
   return (
     <StyledMain>
       <h1>Man</h1>
 
       <StyledSection>
-        <StyledItem>
-          <Image src={shoes1} alt="shoes man" />
-          <p>Nike air force </p>
-          <StyledRight>
-            <p>$300,00</p>
-            <button>add to cart</button>
-          </StyledRight>
-        </StyledItem>
-        <StyledItem>
-          <Image src={shoes1} alt="shoes man" />
-          <p>Nike air force </p>
-          <StyledRight>
-            <p>$300,00</p>
-            <button>add to cart</button>
-          </StyledRight>
-        </StyledItem>
-        <StyledItem>
-          <Image src={shoes1} alt="shoes man" />
-          <p>Nike air force </p>
-          <StyledRight>
-            <p>$300,00</p>
-            <button>add to cart</button>
-          </StyledRight>
-        </StyledItem>
         <StyledItem>
           <Image src={shoes1} alt="shoes man" />
           <p>Nike air force </p>
