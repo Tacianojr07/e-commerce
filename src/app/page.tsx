7ea362d8ce3ac92f08d34ab841f0b4fb1df12313
Tacianjr07 <t.juniior13@gmail.com>
2024-01-06 13:41:41 -0300

    :sparkles: feat: structue initial pagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import shoes1 from "../assets/pexels-aman-jakhar-2048548.jpg";
 export default function Home() {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <main>
+      <h1>Man</h1>
+
+      <section>
+        <div>
+          <Image src={shoes1} alt="shoes man" />
+          <p>Shoes man</p>
+          <div>
+            $300,00
+            <button>add to cart</button>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
