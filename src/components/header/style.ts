344ced12e42574384d31609bef055c5b129a212d
Tacianjr07 <t.juniior13@gmail.com>
2024-01-06 12:26:52 -0300

    :lipstick: feat: styled header

---
+++ src/components/header/style.ts
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  width: 100%;
+  max-width: 1920px;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  height: 80px;
+`;
+
+export const StyledUl = styled.ul`
+  display: flex;
+  gap: 10px;
+
+  a {
+    text-decoration: none;
+    color: #000;
+    opacity: 0.75;
+    transition: 0.4;
+    &:hover {
+      color: #000;
+      opacity: 1;
+    }
+  }
+`;
+
+export const StyledAmount = styled.p`
+  background-color: #454545;
+  border-radius: 100%;
+  position: absolute;
+  padding: 1px 6px;
+  font-size: 13px;
+  right: 13.2%;
+  color: white;
+`;
