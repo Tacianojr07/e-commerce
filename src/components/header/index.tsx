f646c6e2e970320be347d739a48b65d99ff22767
Tacianjr07 <t.juniior13@gmail.com>
2024-01-06 12:25:26 -0300

    :sparkles: feat: creating component header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,26 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { IoBagOutline } from "react-icons/io5";
+import { StyledAmount, StyledHeader, StyledUl } from "./style";
+
+function Header() {
+  return (
+    <StyledHeader>
+      <h1>VENDRE</h1>
+      <nav>
+        <StyledUl>
+          <Link href="#">Man</Link>
+          <Link href="#">Woman</Link>
+        </StyledUl>
+      </nav>
+
+      <div className="circle">
+        <StyledAmount>1</StyledAmount>
+        <IoBagOutline size={28} color="#000" />
+      </div>
+    </StyledHeader>
+  );
+}
+
+export default Header;
