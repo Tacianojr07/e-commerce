6d5a1ccc268cf0b6de898f2fb3c11c7088b8bf1e
Tacianjr07 <t.juniior13@gmail.com>
2024-01-07 19:00:12 -0300

    :sparkles: feat: creating fuction add item cart

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { IProductsProps } from "@/app/page";
 import React, { ReactNode, useState } from "react";
 import { createContext } from "react";
 
@@ -8,6 +9,7 @@ interface IContextProviderProps {
 interface IContextProps {
   cart: ICartProps[];
   CartAmout: number;
+  addItemCart: (newItem: IProductsProps) => void;
 }
 
 interface ICartProps {
@@ -16,17 +18,43 @@ interface ICartProps {
   cover: string;
   price: number;
   description: string;
-  amout: number;
+  amount: number;
   total: number;
 }
 
 export const Context = createContext({} as IContextProps);
 
 function ContextProvider({ children }: IContextProviderProps) {
-  const [cart, setCart1] = useState<ICartProps[]>([]);
+  const [cart, setCart] = useState<ICartProps[]>([]);
+
+  function addItemCart(newItem: IProductsProps) {
+    const indexItem = cart.findIndex((item) => item.id === newItem.id);
+
+    if (indexItem !== -1) {
+      //se if for verdadeiro somamos mais item e calculamos o total
+      const cartList = cart;
+
+      cartList[indexItem].amount = cartList[indexItem].amount + 1;
+      cartList[indexItem].total =
+        cartList[indexItem].amount * cartList[indexItem].price;
+
+      setCart(cartList);
+      return;
+    }
+
+    //adiciona um item na nossa lista
+
+    const data = {
+      ...newItem,
+      amount: 1,
+      total: newItem.price,
+    };
+
+    setCart((prevCart) => [...prevCart, data]);
+  }
 
   return (
-    <Context.Provider value={{ cart, CartAmout: cart.length }}>
+    <Context.Provider value={{ cart, CartAmout: cart.length, addItemCart }}>
       {children}
     </Context.Provider>
   );
