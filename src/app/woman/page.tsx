ba2b13d03c0e5bc88ae45b4ce1c1c1f0203f040f
Tacianjr07 <t.juniior13@gmail.com>
2024-01-09 12:24:14 -0300

    sparkles: feat: creating new page shoes woman

---
+++ src/app/woman/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import Image from "next/image";
+import shoes1 from "../assets/pexels-aman-jakhar-2048548.jpg";
+import { StyledItem, StyledMain, StyledRight, StyledSection } from "../style";
+import { useState, useEffect } from "react";
+import { api } from "@/services/api";
+import { useContext } from "react";
+import { Context } from "@/context/CartContext";
+
+export interface IProductsProps {
+  total: number;
+  id: number;
+  name: string;
+  cover: string;
+  description: string;
+  price: number;
+}
+
+export default function Home() {
+  const { addItemCart } = useContext(Context);
+
+  useEffect(() => {
+    async function getProducuts() {
+      const response = await api.get("/woman");
+      setProducts(response.data);
+    }
+
+    getProducuts();
+  }, []);
+
+  const [products, setProducts] = useState<IProductsProps[]>([]);
+
+  function handleAddItem(products: IProductsProps) {
+    addItemCart(products);
+  }
+  return (
+    <StyledMain>
+      <h1>Woman</h1>
+
+      <StyledSection>
+        {products.map((products) => (
+          <StyledItem key={products.id}>
+            <Image
+              src={products.cover}
+              width={100}
+              height={100}
+              quality={100}
+              alt="shoes Woman"
+              layout="responsive"
+            />
+            <p>{products.name} </p>
+            <StyledRight>
+              <p>${products.price}</p>
+              <button onClick={() => handleAddItem(products)}>
+                add to cart
+              </button>
+            </StyledRight>
+          </StyledItem>
+        ))}
+      </StyledSection>
+    </StyledMain>
+  );
+}
